a97a2ee81167c1e9422272ad406557e610e759d2
AndreyYermakov <yermakovandrey92@gmail.com>
2023-10-04 00:00:13 -0400

    added contact page

--- config/express.js
+++ config/express.js
@@ -22,7 +22,7 @@ module.exports = function () {
     app.set('view engine', 'ejs');
     app.use('/', require('../app/routes/index.server.routes.js'));
     //require('../app/routes/index.server.routes.js')(app);
-    app.use(express.static('./public'));
+    app.use(express.static('./public/'));
     app.use(express.static("./node_modules"));
 
     return app;
